236f65f00f601f5fadabb20d7165df6498f8ab40
JV <jv.jbarros@gmail.com>
2022-07-13 17:59:02 -0300

    Commit First Day

---
+++ Day1.py
@@ -0,0 +1,10 @@
+# Exercise 1 - BAND NAME GENERATOR
+
+import sys
+import re
+import random
+import math
+print("Welcome to the Band Name Generator.")
+city = input("Whats the name of the city you grew up in? \n")
+name = input("Whats your pets name? \n")
+print("Your band could be", name, city)
